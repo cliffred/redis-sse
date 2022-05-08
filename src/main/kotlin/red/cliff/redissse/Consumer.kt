4916a477b6e17d57a4125e2a0d49db5b02198305
Cliffred van Velzen <cliffred@cliffred.nl>
2022-05-08 09:41:50 +0200

    Use Flow instead of Flux

--- src/main/kotlin/red/cliff/redissse/Consumer.kt
+++ src/main/kotlin/red/cliff/redissse/Consumer.kt
@@ -1,8 +1,15 @@
 package red.cliff.redissse
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.redis.core.ReactiveRedisOperations
+import org.springframework.data.redis.core.listenToChannelAsFlow
 import org.springframework.stereotype.Service
 import javax.annotation.PostConstruct
 
@@ -15,11 +22,12 @@ class Consumer(
 
     @PostConstruct
     fun consume() {
-        runBlocking {
-            redisTemplate.listenToChannel(jokeTopic)
-                .map { it.message }
-                .subscribe { println(it) }
-        }
+        redisTemplate.listenToChannelAsFlow(jokeTopic)
+            .map { it.message }
+            .onEach { println(it) }
+            .catch { System.err.println(it.message) }
+            .onCompletion { println("DONE") }
+            .launchIn(CoroutineScope(Dispatchers.Default))
     }
 
 }
