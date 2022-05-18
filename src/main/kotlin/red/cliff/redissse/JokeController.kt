6e2ea5b4e2f72f9659457c387b2dcf5f03460a02
Cliffred van Velzen <cliffred@cliffred.nl>
2022-05-18 16:55:18 +0200

    Use channels instead of sinks

--- src/main/kotlin/red/cliff/redissse/JokeController.kt
+++ src/main/kotlin/red/cliff/redissse/JokeController.kt
@@ -2,52 +2,56 @@ package red.cliff.redissse
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.reactive.asFlow
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Sinks
 import java.util.concurrent.atomic.AtomicInteger
 import javax.annotation.PostConstruct
 
 @RestController
 class JokeController(
-    private val sinkManager: SinkManager
+    private val flowManager: FlowManager
 ) {
     @GetMapping(path = ["/jokes"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun jokes(): Flow<JokeWithId> = sinkManager.newSink()
+    fun jokes(): Flow<JokeWithId> = flowManager.newChannel()
 }
 
 @Component
-class SinkManager(
+class FlowManager(
     private val jokeRepository: JokeRepository
 ) {
     private val id = AtomicInteger(0)
 
-    val sinks = mutableMapOf<Int, Sinks.Many<JokeWithId>>()
+    val channels = mutableMapOf<Int, Channel<JokeWithId>>()
 
-
-    fun newSink(): Flow<JokeWithId> {
-        val sink = Sinks.many().replay().latest<JokeWithId>()
-        sinks[id.incrementAndGet()] = sink
-        return sink.asFlux().asFlow()
+    fun newChannel(): Flow<JokeWithId> {
+        val id = id.incrementAndGet()
+        val channel = Channel<JokeWithId>().apply { invokeOnClose { channels.remove(id) } }
+        channels[id] = channel
+        return channel.consumeAsFlow()
     }
 
     @PostConstruct
     fun subscribe() {
         jokeRepository.listen()
             .onEach { joke ->
-                sinks.forEach { (id, sink) -> sink.tryEmitNext(JokeWithId(id, joke)) }
+                println("Sending to ${channels.size} channels")
+                channels.forEach { (id, channel) ->
+                    channel.send(JokeWithId(id, joke))
+                }
             }
             .catch { it.printStackTrace() }
             .onCompletion { println("DONE") }
-            .launchIn(CoroutineScope(Dispatchers.Default))
+            .launchIn(CoroutineScope(Dispatchers.Default + SupervisorJob()))
     }
 }
 
